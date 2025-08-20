1fd96de77a64bf0780b1e24b8869099dac6b44fd
Gsdagustavo <gugadanielalvez@gmail.com>
2025-08-20 11:56:28 -0300

    feat: add UpdateUserRequest and UpdateUserResponse in user.go

--- internal/models/user.go
+++ internal/models/user.go
@@ -20,3 +20,15 @@ type AddUserRequest struct {
 type AddUserResponse struct {
 	ID int64 `json:"id"`
 }
+
+type UpdateUserRequest struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Phone    string `json:"phone"`
+	IsAdm    bool   `json:"is_adm"`
+}
+
+type UpdateUserResponse struct {
+	ID int64 `json:"id"`
+}
