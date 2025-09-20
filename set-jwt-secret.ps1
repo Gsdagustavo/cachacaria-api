38f7c0e62bc7e39d970080b6d4de7fc7c67b972e
Gsdagustavo <gugadanielalvez@gmail.com>
2025-09-20 13:10:14 -0300

    chore(): Remove unused set-jwt-secret.ps1 file

--- set-jwt-secret.ps1
+++
@@ -1,5 +0,0 @@
-# Set the JWT_SECRET environment variable for the user profile
-[System.Environment]::SetEnvironmentVariable('JWT_SECRET', 'secret', [System.EnvironmentVariableTarget]::User)
-
-# Verify the change by getting the environment variable
-$env:JWT_SECRET
