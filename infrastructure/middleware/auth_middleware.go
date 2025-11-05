5d353f85522c49092f30946d5f023a72716c5679
Gsdagustavo <gugadanielalvez@gmail.com>
2025-11-05 11:41:19 -0300

    fix admin middleware

--- infrastructure/middleware/auth_middleware.go
+++ infrastructure/middleware/auth_middleware.go
@@ -2,40 +2,30 @@ package middleware
 
 import (
 	"cachacariaapi/infrastructure/util"
-	"log"
 	"net/http"
 	"strings"
 )
 
 func AuthMiddlewareWithAdmin(crypt util.Crypt, adminOnly bool) func(http.HandlerFunc) http.HandlerFunc {
-	log.Printf("AuthMiddlewareWithAdmin")
-
 	return func(next http.HandlerFunc) http.HandlerFunc {
-
-		log.Printf("AuthMiddlewareWithAdmin 1st")
-
 		return func(w http.ResponseWriter, r *http.Request) {
-			log.Printf("AuthMiddlewareWithAdmin 2nd")
-
 			authHeader := r.Header.Get("Authorization")
 
 			if authHeader == "" {
-				log.Printf("no token found in request")
 				w.WriteHeader(http.StatusUnauthorized)
 				util.Write(w, util.ServerResponse{
 					Status:  http.StatusUnauthorized,
-					Message: "Token ausente no cabeçalho Authorization",
+					Message: "Sem autorização",
 				})
 				return
 			}
 
 			parts := strings.SplitN(authHeader, " ", 2)
 			if len(parts) != 2 || !strings.EqualFold(parts[0], "Bearer") {
-				log.Printf("invalid token found in request")
 				w.WriteHeader(http.StatusUnauthorized)
 				util.Write(w, util.ServerResponse{
 					Status:  http.StatusUnauthorized,
-					Message: "Formato de token inválido. Use 'Bearer <token>'",
+					Message: "Sem autorização",
 				})
 				return
 			}
@@ -43,23 +33,19 @@ func AuthMiddlewareWithAdmin(crypt util.Crypt, adminOnly bool) func(http.Handler
 			token := parts[1]
 			payload, err := crypt.VerifyAuthToken(token)
 			if err != nil {
-				log.Printf("invalid/expired token found in request")
 				w.WriteHeader(http.StatusUnauthorized)
 				util.Write(w, util.ServerResponse{
 					Status:  http.StatusUnauthorized,
-					Message: "Token inválido ou expirado",
+					Message: "Sem autorização",
 				})
 				return
 			}
-
-			log.Printf("admin only: %v", adminOnly)
-			log.Printf("is admin: %v", payload.IsAdmin)
 
 			if adminOnly && !payload.IsAdmin {
 				w.WriteHeader(http.StatusUnauthorized)
 				util.Write(w, util.ServerResponse{
 					Status:  http.StatusForbidden,
-					Message: "Acesso negado: apenas administradores podem acessar esta rota",
+					Message: "Sem autorização",
 				})
 				return
 			}
