516f23d6279960c01da11cc0822a2b76aec2e548
Gsdagustavo <gugadanielalvez@gmail.com>
2025-11-17 11:24:31 -0300

    chore(): Minor fixes

--- infrastructure/infrastructure.go
+++ infrastructure/infrastructure.go
@@ -2,13 +2,11 @@ package infrastructure
 
 import (
 	"cachacariaapi/domain/usecases"
-	util2 "cachacariaapi/domain/util"
 	"cachacariaapi/infrastructure/config"
 	"cachacariaapi/infrastructure/datastore/repositories"
 	"cachacariaapi/infrastructure/modules"
 	"cachacariaapi/infrastructure/util"
 	"fmt"
-	"log"
 	"log/slog"
 	"os"
 
@@ -44,7 +42,7 @@ func Init() {
 	cartRepository := repositories.NewMySQLCartRepository(conn)
 
 	// Use Cases
-	authUseCases := usecases.NewAuthUseCases(authRepository, userRepository, authManager)
+	authUseCases := usecases.NewAuthUseCases(authRepository, userRepository, authManager, cfg.Email)
 	userUseCases := usecases.NewUserUseCases(userRepository, authRepository, authManager)
 	productUseCases := usecases.NewProductUseCases(productRepository, cfg.Server.BaseURL)
 	cartUseCases := usecases.NewCartUseCases(cartRepository, userRepository, productRepository, cfg.Server.BaseURL)
@@ -70,18 +68,6 @@ func Init() {
 	cfg.Server.RegisterModules(apiSubrouter, authModule, userModule, productModule, cartModule)
 
 	slog.Info(fmt.Sprintf("server running on port %d", cfg.Server.Port))
-
-	log.Printf("STMP host: %s", cfg.Email.SMTPHost)
-	log.Printf("STMP port: %s", cfg.Email.SMTPPort)
-	log.Printf("From: %s", cfg.Email.From)
-	log.Printf("Username: %s", cfg.Email.Username)
-	log.Printf("Password: %s", cfg.Email.Password)
-
-	err = util2.SendEmail(cfg.Email, []string{"gugadanielalvez@gmail.com"}, "Welcome!", "Your account has been created!")
-	if err != nil {
-		slog.Error("failed to send email", "error", err)
-		os.Exit(1)
-	}
 
 	err = cfg.Server.Run(*cfg)
 
